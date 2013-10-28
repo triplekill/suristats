c3fad2081ea497f99f0f2fe841fba3f6f4fb4419
david <david@localhost.localdomain>
2013-10-28 14:16:35 +0100

    Lundi 28 octobre 2013

---
+++ suristats.h
@@ -0,0 +1,14 @@
+/*
+ * suristats.h
+ *
+ *  Created on: 28 oct. 2013
+ *      Author: david
+ */
+
+#ifndef SURISTATS_H_
+#define SURISTATS_H_
+
+#define	TRUE	1
+#define	FALSE	0
+
+#endif /* SURISTATS_H_ */
